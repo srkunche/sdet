d4fbee8c24698f2e88dd69c419b74720efe3e035
srkunche <75238880+srkunche@users.noreply.github.com>
2020-12-02 09:07:33 +0530

    Add files via upload

---
+++ java/Activity1_1.java
@@ -0,0 +1,21 @@
+package JavaActivities;
+
+public class Activity1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//create an Object of the Car class
+		Car maruthi=new Car();
+		maruthi.make=2014;
+		maruthi.color="Black";
+		maruthi.transmission="Manual";
+		//Call the methods 
+		maruthi.displayCharacterstics();
+		maruthi.accelerate();
+		maruthi.brake();
+		
+		
+
+	}
+
+}
